aafbfe6295b81ebe1a2e26a779751ff87c1208bc
Technophile <dostonnabotov@gmail.com>
2025-06-16 02:30:56 +0300

    Remove syntax highlighter in code editor part

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,32 +1,15 @@
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
-
 type CodeEditorProps = {
   setRawCode: (code: string) => void;
   rawCode: string;
-  codeLanguage: string;
 };
 
-export default function CodeEditor({
-  codeLanguage,
-  rawCode,
-  setRawCode,
-}: CodeEditorProps) {
+export default function CodeEditor({ rawCode, setRawCode }: CodeEditorProps) {
   const handleInput = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setRawCode(event.target.value);
   };
 
   return (
     <div className="code-editor">
-      <SyntaxHighlighter
-        language={codeLanguage}
-        style={oneDark}
-        customStyle={{ margin: 0, pointerEvents: "none" }}
-        className="code-editor__syntax-highlighter"
-        wrapLines={true}
-      >
-        {rawCode}
-      </SyntaxHighlighter>
       <textarea
         value={rawCode}
         onChange={handleInput}
